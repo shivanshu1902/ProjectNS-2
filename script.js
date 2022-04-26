9f59a9a149269cbcdf4d91622f65c414fe8442f8
Your Name <you@example.com>
2022-04-26 23:22:19 +0530

    Change

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-function age() {
-    var d1 = document.getElementById('date').value;
-    var m1 = document.getElementById('month').value;
-    var y1 = document.getElementById('year').value;
-  
-    var date = new Date();
-    var d2 = date.getDate();
-    var m2 = 1 + date.getMonth();
-    var y2 = date.getFullYear();
-    var month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-  
-    if(d1 > d2){
-        d2 = d2 + month[m2 - 1];
+function calculate(){
+    let dob = document.getElementById("input").value;
+    let bday= new Date(dob);
+    let d1 = bday.getDate();
+    let m1 = bday.getMonth();
+    let y1 = bday.getFullYear();
+    
+    let today = new Date();
+    let d2 = today.getDate();
+    let m2 = today.getMonth();
+    let y2 = today.getFullYear();
+
+    const month = [31,28,31,30,31,30,31,31,30,31,30,31];
+
+    let leap= Math.floor((y2-y1)/4);
+    d2 += leap;
+    
+
+    if(d1>d2){
+        d2 = d2 + month[m2];
         m2 = m2 - 1;
     }
-    if(m1 > m2){
+    if(m1>m2){
         m2 = m2 + 12;
         y2 = y2 - 1;
     }
-    var d = d2 - d1;
-    var m = m2 - m1;
-    var y = y2 - y1;
-  
-    document.getElementById('age').innerHTML = 'Your Age is '+y+' Years '+m+' Months '+d+' Days';
-  }
+    
+    if(bday.getTime() <= today.getTime()){
+        (document.getElementById("days").innerHTML = d2 - d1);
+        (document.getElementById("years").innerHTML = y2 - y1);
+        (document.getElementById("months").innerHTML = m2 - m1);
+    }else{
+        
+        (document.getElementById("years").innerHTML ="not");
+        (document.getElementById("months").innerHTML = "born");
+        (document.getElementById("days").innerHTML = "yet");
+    }
+
+}
